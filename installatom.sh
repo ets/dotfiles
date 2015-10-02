f02e32b9f296f953951290d5fc62273df56ee3ec
Eric Simmerman <ericsimmerman@gmail.com>
2015-10-02 13:58:22 -0400

    Atom packages

---
+++ installatom.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# install Atom and plugins
+cd ~/workspace
+git clone https://github.com/atom/atom.git
+cd atom
+script/build # Creates application at /Applications/Atom.app
+apm install file-watcher
