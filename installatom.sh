fadc7b81a13b6185b21af2d82aabf51d6ed9b854
Eric Simmerman <ericsimmerman@gmail.com>
2015-12-01 21:13:04 -0500

    update repo address

--- installatom.sh
+++ installatom.sh
@@ -2,7 +2,8 @@
 
 # install Atom and plugins
 cd ~/workspace
-git clone https://github.com/ets/atom/atom.git
+git clone https://github.com/ets/atom.git
 cd atom
 script/build # Creates application at /Applications/Atom.app
 apm install file-watcher
+apm install wordcount
