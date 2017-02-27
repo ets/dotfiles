f8b5ad675b53a7755af302e6e2fdfe73066c26ee
Eric Simmerman <ericsimmerman@gmail.com>
2017-02-26 21:12:02 -0500

    Atom updates

--- installatom.sh
+++ installatom.sh
@@ -3,8 +3,12 @@
 curdir=`pwd`
 # install Atom and plugins
 cd ~/workspace
+if [ -d atom ]; then
+cd atom; git pull
+else
 git clone https://github.com/ets/atom.git
 cd atom
+fi
 script/build # Creates application at /Applications/Atom.app
 apm install file-watcher
 apm install wordcount
@@ -17,5 +21,6 @@ apm install vim-mode-plus
 apm install file-icons
 apm install dash
 apm install monokai-seti
+echo "Install https://fonts.google.com/?query=Incon&selection.family=Inconsolata"
 
 cp ${curdir}/.atom/* ~/.atom
