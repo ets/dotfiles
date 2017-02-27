6d96632cc752a2ca19a4014a307fd7873b2dd607
Eric Simmerman <ericsimmerman@gmail.com>
2017-02-26 21:13:16 -0500

    Remove vim mode

--- installatom.sh
+++ installatom.sh
@@ -17,7 +17,7 @@ apm install sublime-style-column-selection
 apm install language-docker
 apm install go-plus
 apm install go-rename
-apm install vim-mode-plus
+# no likey apm install vim-mode-plus
 apm install file-icons
 apm install dash
 apm install monokai-seti
